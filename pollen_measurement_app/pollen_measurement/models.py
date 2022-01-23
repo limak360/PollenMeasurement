fd60114b2c144a4dacb5ca79e8c6999524e52a9d
I543517 <kamil.jacko@sap.com>
2022-01-23 23:40:27 +0100

    03 Created pollen measurement model

--- pollen_measurement_app/pollen_measurement/models.py
+++ pollen_measurement_app/pollen_measurement/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class PollenMeasurement(models.Model):
+    pollen_type = models.CharField(max_length=100)
+    pollen_count = models.CharField(max_length=200)
+    measurement_location = models.CharField(max_length=200)
+    measurement_time = models.DateTimeField('measurement_time')
